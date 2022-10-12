f8f4fbc648e837769a79742157321560f851d42e
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-12 12:58:11 +0530

    service

---
+++ src/app/api.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+
+
+
+
+viewdatalinks=()=>{
+  return this.http.get("https://api.publicapis.org/entries")
+}
+
+
+}
