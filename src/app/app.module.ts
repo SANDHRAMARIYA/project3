4fee5339f428a86dfeebc9024c91d0f28c6457df
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-12 12:45:33 +0530

    datalinks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DatalinksComponent } from './datalinks/datalinks.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+
+const appRoutes:Routes=[
+  {
+    path:"",component:DatalinksComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DatalinksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
